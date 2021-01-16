aadfec7f202e801e5d247420c953d17141574d0c
ryanrwillis <ryan.willis@hotmail.ca>
2021-01-16 16:18:05 -0500

    added remaining endpoints

--- emotion_server/server.py
+++ emotion_server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
+import text2emotion as te
 
 now = datetime.now()
 app = Flask(__name__)
@@ -26,11 +27,11 @@ video_emotion_stack = [
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@app.route('/give/stt', methods=['POST'])
-def get_text():
-    req_data = request.form.to_dict()['text']
-    print(req_data)
-    return 'test'
+# @app.route('/give/stt', methods=['POST'])
+# def give_text():
+#     req_data = request.form.to_dict()['text']
+#     print(req_data)
+#     return 'test'
 
 @app.route('/give/video-emotions', methods=['POST'])
 def give_video_emotions():
@@ -40,5 +41,27 @@ def give_video_emotions():
 @app.route('/get/video-emotions', methods=['GET'])
 def get_video_emotions():
     return video_emotion_stack[-1][0]
+
+@app.route('/give/wpm', methods=['POST'])
+def give_wpm():
+    wpm_stack.append([request.form.to_dict(), datetime.now()])
+    return 200
+
+@app.route('/get/wpm', methods=['GET'])
+def get_wpm():
+    return wpm_stack[-1][0]
+
+@app.route('/give/text', methods=['POST'])
+def give_text():
+    text_stack.append([request.form.to_dict(), datetime.now()])
+    return 200
+
+@app.route('/get/text', methods=['GET'])
+def get_text():
+    return text_stack[-1][0]
+
+@app.route('/get/text_emotion', methods=['GET'])
+def get_text_emotion():
+    return te.get_emotion(text_stack[-1][0])
 
 app.run(port=5555)
