58e3092a193f3152bb3b02a59c37761e1064cbc4
Bill Liu <billliusiyi@gmail.com>
2021-01-17 05:10:10 -0500

    words per minute

--- frontend/material-dashboard-react-master/src/Emotions.js
+++ frontend/material-dashboard-react-master/src/Emotions.js
@@ -12,11 +12,8 @@ const Emotions = ({ emotions }) => {
     return (
         <GridContainer>
             {emotions.map((emotion) => (
-                <GridItem xs={12} sm={6} md={3} style={{ backgroundColor: "white"}}>
+                <GridItem xs={12} sm={6} md={3} style={{ marginTop: "15px", marginBottom: "15px"}}>
                     <Card className="card" key={emotion['emotion']}>
-                        <CardIcon color="danger">
-                            <Icon><span role="img" aria-label="happy">😃</span></Icon>
-                        </CardIcon>
                         <div className="card-body" style={{ display: "grid", placeItems: "center" }}>
                             <h5 className="card-title">{emotion['emotion']}</h5>
                             <h4 className="card-subtitle mb-2 text-muted">{parseFloat(emotion['value']).toFixed(6)}</h4>
@@ -24,6 +21,14 @@ const Emotions = ({ emotions }) => {
                     </Card>
                 </GridItem>
             ))}
+            <GridItem xs={12} sm={6} md={3} style={{ marginTop: "15px", marginBottom: "15px"}}>
+                    <Card className="card" key={WPM['WPM']}>
+                        <div className="card-body" style={{ display: "grid", placeItems: "center" }}>
+                            <h5 className="card-title">{WPM['WPM']}</h5>
+                            <h4 className="card-subtitle mb-2 text-muted">{parseFloat(WPM['WPM']).toFixed(3)}</h4>
+                        </div>
+                    </Card>
+                </GridItem>
         </GridContainer>
     )
 };
