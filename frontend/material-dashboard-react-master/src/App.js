877c33437e01c6d05d7eae8b47d8e782b68e7622
Bill Liu <billliusiyi@gmail.com>
2021-01-17 05:40:05 -0500

    clear merge conflict

--- frontend/material-dashboard-react-master/src/App.js
+++ frontend/material-dashboard-react-master/src/App.js
@@ -8,7 +8,7 @@ import './VideoScript.js';
 import './stylesheet.css';
 import logo from './assets/img/logo.png';
 import GridItem from "./components/Grid/GridItem";
-import {Card} from "@material-ui/core";
+import { Card } from "@material-ui/core";
 import GridContainer from "./components/Grid/GridContainer";
 
 class App extends Component {
@@ -24,30 +24,30 @@ class App extends Component {
             <div style={{ display: "grid", placeItems: "center" }}>
                 <center><img src={logo} alt="monolog" width="240px" height="80px" /></center>
                 <VideoFeed />
-                <Emotions emotions={this.state.emotions} wpm={this.state.wpm}/>
-                            <GridItem xs={12} sm={6} md={3} style={{ marginTop: "15px", marginBottom: "15px"}}>
+                <button
+                    style={{
+                        width: "180px",
+                        height: "40px",
+                        borderRadius: "10px",
+                        letterSpacing: "1px",
+                        marginTop: "1rem",
+                        marginBottom: '1rem'
+                    }}
+                    className="btn btn-small waves-effect waves-light teal"
+                    onClick={this.handleStart}
+                >
+                    {'START'}
+                </button>
+                
+                <Emotions emotions={this.state.emotions} wpm={this.state.wpm} />
+                <GridItem xs={12} sm={6} md={3} style={{ marginTop: "15px", marginBottom: "15px" }}>
                     <Card className="card" key='WPM'>
                         <div className="card-body" style={{ display: "grid", placeItems: "center" }}>
                             <h5 className="card-title">{'WPM'}</h5>
-                            <h4 className="card-subtitle mb-2 text-muted">{parseFloat(this.state.wpm).toFixed(3)}</h4>
+                            <h4 className="card-subtitle mb-2 text-muted">{parseFloat(this.state.wpm).toFixed(2)}</h4>
                         </div>
                     </Card>
                 </GridItem>
-
-                            <button
-                style={{
-                    width: "180px",
-                    borderRadius: "3px",
-                    letterSpacing: "1px",
-                    marginTop: "0.5rem",
-                    marginBottom: '0.1rem'
-                }}
-                className="btn btn-small waves-effect waves-light teal"
-                onClick={this.handleStart}
-            >
-                {'START'}
-            </button>
-
                 <Graph emotionObject={this.state.emotionObject} />
             </div>
         )
