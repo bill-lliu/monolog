58010777cc300c54160f534b892e02ed6efb730e
Bill Liu <billliusiyi@gmail.com>
2021-01-17 03:49:47 -0500

    button

--- frontend/material-dashboard-react-master/src/App.js
+++ frontend/material-dashboard-react-master/src/App.js
@@ -9,6 +9,14 @@ import './stylesheet.css';
 
 class App extends Component {
 
+    // func1() {
+    //     alert("Great Shot!");
+    // }
+
+    // func2() {
+    //     alert("Great Shot!");
+    // }
+
     state = {
         emotions: [],
         emotionObject: []
@@ -43,6 +51,8 @@ class App extends Component {
         return (
             <div style={{ display: "grid", placeItems: "center" }}>
                 <center><img src="frontend\material-dashboard-react-master\src\assets\img\logo.png" alt="monolog" /></center>
+                <button onClick={func1}>Start Recording!</button>
+                <button onClick={func2}>Stop</button>
                 <VideoFeed />
                 <Emotions emotions={this.state.emotions} />
                 <Graph emotionObject={this.state.emotionObject} />
