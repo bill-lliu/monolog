97da87d200f9097f9d92a735afcca52487d08b9f
m-avolio <Matthew.Avolio@hotmail.com>
2021-01-17 04:10:37 -0500

    deleted files

--- frontend/material-dashboard-react-master/src/index.js
+++ frontend/material-dashboard-react-master/src/index.js
@@ -20,10 +20,6 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-// core components
-import Admin from "layouts/Admin.js";
-import RTL from "layouts/RTL.js";
-
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 import App from './App';
 
