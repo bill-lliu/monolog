68ccf4bcf4ebf56e64f0b53fc8a9056f7119537f
Bill Liu <billliusiyi@gmail.com>
2021-01-17 00:02:30 -0500

    updated visuals slightly

--- frontend/material-dashboard-react-master/src/Emotions.js
+++ frontend/material-dashboard-react-master/src/Emotions.js
@@ -15,12 +15,12 @@ const Emotions = ({ emotions }) => {
         <div>
             <GridContainer>
                 {emotions.map((emotion) => (
-                    <GridItem xs={12} sm={6} md={3} style={{backgroundColor:"white", margin:"10px"}}>
+                    <GridItem xs={12} sm={6} md={3} style={{ backgroundColor: "white", margin: "10px" }}>
                         <Card class="card" key={emotion['emotion']}>
                             <CardIcon color="danger">
-                                <Icon><span role="img" aria-label="angry">😠</span></Icon>
+                                <Icon><span role="img" aria-label="happy">😃</span></Icon>
                             </CardIcon>
-                            <div class="card-body" style={{display:"grid", placeItems:"center"}}>
+                            <div class="card-body" style={{ display: "grid", placeItems: "center" }}>
                                 <h5 class="card-title">{emotion['emotion']}</h5>
                                 <h4 class="card-subtitle mb-2 text-muted">{parseFloat(emotion['value']).toFixed(6)}</h4>
                             </div>
